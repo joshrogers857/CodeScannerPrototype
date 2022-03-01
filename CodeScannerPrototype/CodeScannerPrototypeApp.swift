1ce4e3241e2e35fe0f03d74cbfdebc346edf6308
joshrogers857 <87278992+joshrogers857@users.noreply.github.com>
2022-03-01 16:32:00 +0000

    Initial Commit

---
+++ CodeScannerPrototype/CodeScannerPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeScannerPrototypeApp.swift
+//  CodeScannerPrototype
+//
+//  Created by Joshua Rogers on 01/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeScannerPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
